5b177232e491942fd37ad1e4d887b526ff471687
Dawson-Stutzman <dtstutzman@ksu.edu>
2024-04-10 12:07:24 -0500

    Inital Commit

---
+++ CIS560FinalProject/Pages/MetricsByCollegeForm.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CIS560FinalProject.Pages
+{
+    public class MetricsByCollegeFormModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public MetricsByCollegeFormModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
