920f6f6e7f9c981940493a48eb55263e3f718065
Dawson-Stutzman <dtstutzman@ksu.edu>
2024-04-24 15:29:14 -0500

    Matt's Push

--- CIS560FinalProject/Pages/Analytics/PlayerComparisonForm.cshtml.cs
+++ CIS560FinalProject/Pages/Analytics/PlayerComparisonForm.cshtml.cs
@@ -1,50 +1,154 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Data.SqlClient;
-using System.Runtime.InteropServices;
 
-namespace CIS560FinalProject.Pages.Analytics
+public class PlayerComparisonFormModel : PageModel
 {
-
-    public class PlayerComparisonFormModel : PageModel
+    public List<Player> Players;
+    public string AllPlayers;
+    public string Descending;
+    public void OnGet()
     {
-        public List<string> Dummy;
-        public void OnGet()
+        AllPlayers = (HttpContext.Request.Query["allPlayers"].ToString() == "on") ? "checked" : "unchecked";
+        Descending = (HttpContext.Request.Query["descending"].ToString() == "on") ? "checked" : "unchecked";
+        SqlConnection connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=NBA_Statistics;Integrated Security=True");
+        string checkbox = (HttpContext.Request.Query["allPlayers"].ToString());
+        connection.Open();
+
+        #region Format Player Names
+        // Original string of names
+        string players = (HttpContext.Request.Query["players"].ToString());
+        // Split the string into individual names
+        string[] names = players.Split(',');
+        // Format each name with single quotes
+        string[] formattedNames = names.Select(name => $"'{name.Trim()}'").ToArray();
+        // Join the formatted names into a single string
+        string formattedString = string.Join(", ", formattedNames);
+        #endregion
+
+        string rankString;
+        string orderBy;
+        string groupBy = "GROUP BY P.PlayerID, P.PlayerName, P.Verified";
+
+        switch (HttpContext.Request.Query["metric"].ToString())
         {
-            //================================Collecting Relevant Form Data================================
-            string colleges = string.Join(",", HttpContext.Request.Query["colleges"].ToString().Split(',').Select(str => $"'{str.Trim()}'"));
-            string allColleges = (HttpContext.Request.Query["allColleges"].ToString() == "on") ? "checked" : "";
-            string descending = (HttpContext.Request.Query["descending"].ToString() == "on") ? "checked" : "";
-            //================================Parse Data into Appropriate SQL filters================================
-            int filters = 0;
-            string filterString = String.Format("WHERE P.School IN ({0})", colleges);
-            
+            case "points":
+                orderBy = " ORDER BY Points DESC";
+                rankString = "PS.Points";
+                break;
+            case "assists":
+                orderBy = " ORDER BY Assists DESC";
+                rankString = "PS.Assists";
+                break;
+            case "rebounds":
+                orderBy = " ORDER BY Rebounds DESC";
+                rankString = "PS.Rebounds";
+                break;
+            case "blocks":
+                orderBy = " ORDER BY Blocks DESC";
+                rankString = "PS.Blocks";
+                break;
+            case "steals":
+                orderBy = " ORDER BY Steals DESC";
+                rankString = "PS.Steals";
+                break;
+            case "minutes":
+                orderBy = " ORDER BY Minutes DESC";
+                rankString = "PS.Minutes";
+                break;
+            case "turnovers":
+                orderBy = " ORDER BY Turnovers DESC";
+                rankString = "PS.Turnovers";
+                break;
+            default:
+                orderBy = " ORDER BY Points DESC";
+                rankString = "PS.Points";
+                break;
+        }
 
+        string defaultQuery = "WITH RankedPlayers AS (SELECT P.PlayerName AS [Name]," +
+            "   CAST(AVG(PS.Points) AS DECIMAL(10,2)) AS Points," +
+            "   CAST(AVG(PS.Assists) AS DECIMAL(10,2)) AS Assists," +
+            "   CAST(AVG(PS.Rebounds) AS DECIMAL(10,2)) AS Rebounds," +
+            "   CAST(AVG(PS.Blocks) AS DECIMAL(10,2)) AS Blocks," +
+            "   CAST(AVG(PS.Steals) AS DECIMAL(10,2)) AS Steals," +
+            "   CAST(AVG(PS.Minutes) AS DECIMAL(10,2)) AS [Minutes]," +
+            "   CAST(AVG(PS.Turnovers) AS DECIMAL(10,2)) AS Turnovers," +
+            "   P.Verified," +
+            "   ROW_NUMBER() OVER (ORDER BY AVG(" + rankString + ") DESC) AS [Rank]" +
+            "       FROM [Statistics].Player P" +
+            "           INNER JOIN [Statistics].PlayerSeason PS ON PS.PlayerID = P.PlayerID" +
+            "       GROUP BY P.PlayerID, P.PlayerName, P.Verified)" +
+            "SELECT [Rank], [Name], Points, Assists, Rebounds, Blocks, Steals, [Minutes], Turnovers, Verified " +
+            "FROM RankedPlayers " + orderBy;
 
-            SqlConnection connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=NBA;Integrated Security=True");
-            string selectString = String.Format("SELECT * " +
-                                  "FROM [Statistics].PlayerSeason PS" +
-                                  "INNER JOIN [Statistics].Player P ON P.PlayerID = PS.PlayerID" +
-                                        "{0}" +
-                                  "GROUP BY P.SCHOOL" +
-                                  "ORDER BY {1}", filterString, (descending == "checked") ? "DESC" : "ASC");
-            connection.Open();
-            SqlCommand comm = new SqlCommand(selectString, connection);
-            SqlDataReader reader = comm.ExecuteReader();
-            
+        if (players != "" && checkbox == "")
+        {
+            string where = "WHERE [Name] IN (" + formattedString + ") ";
 
-            /*
-            while (reader.Read())
-            {
-                TeamInfo team = new TeamInfo();
-                team.TeamID = reader.GetInt32(0);
-                team.Name = reader.GetString(1);
-                team.ConferenceName = reader.GetString(2);
-                Console.WriteLine(team.Name.ToString());
-            }
-            connection.Close();
-            */
+            defaultQuery = "WITH RankedPlayers AS (SELECT P.PlayerName AS [Name]," +
+            "   CAST(AVG(PS.Points) AS DECIMAL(10,2)) AS Points," +
+            "   CAST(AVG(PS.Assists) AS DECIMAL(10,2)) AS Assists," +
+            "   CAST(AVG(PS.Rebounds) AS DECIMAL(10,2)) AS Rebounds," +
+            "   CAST(AVG(PS.Blocks) AS DECIMAL(10,2)) AS Blocks," +
+            "   CAST(AVG(PS.Steals) AS DECIMAL(10,2)) AS Steals," +
+            "   CAST(AVG(PS.Minutes) AS DECIMAL(10,2)) AS [Minutes]," +
+            "   CAST(AVG(PS.Turnovers) AS DECIMAL(10,2)) AS Turnovers," +
+            "   P.Verified," +
+            "   ROW_NUMBER() OVER (ORDER BY AVG(" + rankString + ") DESC) AS [Rank]" +
+            "       FROM [Statistics].Player P" +
+            "           INNER JOIN [Statistics].PlayerSeason PS ON PS.PlayerID = P.PlayerID" +
+            "       GROUP BY P.PlayerID, P.PlayerName, P.Verified)" +
+            "SELECT [Rank], [Name], Points, Assists, Rebounds, Blocks, Steals, [Minutes], Turnovers, Verified " +
+            "FROM RankedPlayers " + where + orderBy;
 
         }
+
+
+
+        if (players != "" || checkbox != "")
+        {
+            SqlCommand comm = new SqlCommand(defaultQuery, connection);
+            SqlDataReader reader = comm.ExecuteReader();
+            Players = new();
+
+
+            while (reader.Read())
+            {
+                Player p = new Player();
+                p.Rank = reader.GetInt64(0);
+                p.Name = reader.GetString(1);
+                p.Points = reader.GetDecimal(2);
+                p.Assists = reader.GetDecimal(3);
+                p.Rebounds = reader.GetDecimal(4);
+                p.Blocks = reader.GetDecimal(5);
+                p.Steals = reader.GetDecimal(6);
+                p.Minutes = reader.GetDecimal(7);
+                p.Turnovers = reader.GetDecimal(8);
+                p.Verified = reader.GetInt32(9);
+
+
+                Players.Add(p);
+
+            }
+        }
+
+        connection.Close();
+
+
+    }
+
+    public class Player
+    {
+        public string Name;
+        public decimal Points;
+        public decimal Assists;
+        public decimal Rebounds;
+        public decimal Blocks;
+        public decimal Steals;
+        public decimal Minutes;
+        public decimal Turnovers;
+        public int Verified;
+        public long Rank;
+
     }
 }
--- CIS560FinalProject/obj/Debug/net7.0/CIS560FinalProject.AssemblyInfo.cs
+++ CIS560FinalProject/obj/Debug/net7.0/CIS560FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CIS560FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd3abba5595df910cbf870a32f5bdad456c28bbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f9a2fc619670fca50bbb7d94af34fc63feb33ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("CIS560FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CIS560FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
